97cd8462506d7ff628a5e01d6ac7a9a5ad5a6bde
Robert Golawski <robert.golawski@yahoo.co.uk>
2025-08-01 23:00:15 +0100

    language servers and right arrow to decline

--- init.lua
+++ init.lua
@@ -716,6 +716,8 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
+        cssls = {},
+        zls = {},
         -- pyright = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -881,6 +883,9 @@ require('lazy').setup({
           ['<C-n>'] = cmp.mapping.select_next_item(),
           -- Select the [p]revious item
           ['<C-p>'] = cmp.mapping.select_prev_item(),
+
+          -- Decline and close suggestions
+          ['<right>'] = cmp.mapping.abort(),
 
           -- Scroll the documentation window [b]ack / [f]orward
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
